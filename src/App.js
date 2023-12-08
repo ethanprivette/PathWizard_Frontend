eb71136b56c3c759968050bd29e207d62d68a207
ethanprivette <dabosses444@gmail.com>
2023-12-07 23:06:03 -0500

    ui started testing dropdown menus

--- src/App.js
+++ src/App.js
@@ -1,30 +1,25 @@
-import logo from './logo.svg';
 import './App.css';
-
-function MyButton() {
-  return (
-    <button>I'm aa button</button>
-  );
-}
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-        <MyButton />
-      </header>
+      <div class="sidenav">
+        <a href="#">Waypoints</a>
+        <a href="#">Global Constraints</a>
+        <a href="#">End Velocity</a>
+        <a href="#">Event Markers</a>
+        <a href="#">Constraint Zones</a>
+        <a href="#">Reversed</a>
+
+        <li class="dropdown dropdown-6">Test
+          <ul class="dropdown_menu dropdown_menu--animated dropdown_menu-6">
+            <li class="dropdown_item-1">test1</li>
+            <li class="dropdown_item-2">test2</li>
+            <li class="dropdown_item-3">test3</li>
+            <li class="dropdown_item-4">test4</li>
+          </ul>        
+        </li>
+      </div>
     </div>
   );
 }
